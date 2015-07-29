9818cfc474d33adef06184e559290cf3154769b5
skaliak <info@skaliak.com>
2015-07-28 23:55:45 -0700

    tested serializing json

--- ExprTreesTests/UnitTest1.cs
+++ ExprTreesTests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ExprTreesTests
 {
@@ -9,6 +11,28 @@ namespace ExprTreesTests
         [TestMethod]
         public void TestMethod1()
         {
+            //should be a list at the second level
+            var x = new List<Tuple<Tuple<string, string, string, int>, int>>();
+
+            x.Add(Tuple.Create(Tuple.Create("hey", "ho", "poo", (int)color.blue), (int)color.red));
+            x.Add(Tuple.Create(Tuple.Create("hoi", "blah", "foo", (int)ord.one), (int)ord.two));
+
+            var json = JsonConvert.SerializeObject(x);
+
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
         }
+    }
+
+    enum color
+    {
+        red,
+        blue
+    }
+
+    enum ord
+    {
+        one = 2,
+        two = 3
     }
 }
